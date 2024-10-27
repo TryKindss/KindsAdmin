e2c89c426ee2c86b0275fc7ec803c98319084f13
Earl <earlpappi@gmail.com>
2024-10-27 11:01:26 +0100

    chore: account selection done

--- src/app/scaffold/navigation/account-selection.tsx
+++ src/app/scaffold/navigation/account-selection.tsx
@@ -1,6 +1,14 @@
 "use client";
 import * as React from "react";
-import { Search, ChevronUp, ChevronDown, Settings, Plus } from "lucide-react";
+import {
+  Search,
+  ChevronUp,
+  ChevronDown,
+  Settings,
+  Plus,
+  ArrowUp,
+  ArrowDown,
+} from "lucide-react";
 import { Avatar } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
@@ -41,10 +49,10 @@ export default function SelectAccount() {
     <>
       <DialogContent
         hideClose
-        className="sm:max-w-[425px] md:max-w-lg bg-white bg-opacity-70 p-0"
+        className="sm:max-w-[425px] md:max-w-2xl bg-white bg-opacity-70 p-0"
       >
         <div className="grid gap-4">
-          <div className="relative border-b-2 border-kindsGrey flex items-center gap-0 py-2 px-6">
+          <div className="relative border-b-2 border-kindsGrey flex items-center gap-0 pt-2 px-6">
             <Search className=" h-4 w-4 text-muted-foreground" />
             <Input
               placeholder="Search accounts"
@@ -52,44 +60,47 @@ export default function SelectAccount() {
             />
           </div>
 
-          <section className="px-6">
+          <ScrollArea className="max-h-[480px]">
             <div>
-              <h4 className="mb-4 text-sm font-medium">Recent</h4>
-              <div className="space-y-4">
+              <h4 className="mb-4 text-sm font-medium px-4">Recent</h4>
+              <div className="">
                 {accounts.map((account) => (
                   <AccountItem key={account.name} account={account} />
                 ))}
               </div>
             </div>
             <div>
-              <h4 className="mb-4 text-sm font-medium">All Accounts</h4>
-              <ScrollArea className="h-[200px]">
-                <div className="space-y-4">
-                  {accounts.map((account) => (
-                    <AccountItem key={account.name} account={account} />
-                  ))}
-                </div>
-              </ScrollArea>
-            </div>
-            <div className="flex items-center justify-between text-sm text-muted-foreground">
-              <div className="flex items-center space-x-2">
-                <ChevronUp className="h-4 w-4" />
-                <ChevronDown className="h-4 w-4" />
-                <span>to navigate</span>
-              </div>
-              <div className="flex items-center space-x-4">
-                <span>esc to close</span>
-                <Button variant="outline" size="sm" className="h-8 px-2">
-                  <Settings className="mr-2 h-4 w-4" />
-                  Manage Accounts
-                </Button>
-                <Button variant="outline" size="sm" className="h-8 px-2">
-                  <Plus className="mr-2 h-4 w-4" />
-                  Create New
-                </Button>
+              <h4 className="mb-4 text-sm font-medium px-4">All Accounts</h4>
+              <div className="">
+                {accounts.map((account) => (
+                  <AccountItem key={account.name} account={account} />
+                ))}
               </div>
             </div>
-          </section>
+          </ScrollArea>
+          <div className="flex items-center justify-between text-sm text-muted-foreground py-2 border-t-2 border-kindsGrey px-4">
+            <div className="flex items-center space-x-2">
+              <div className="bg-white rounded-lg p-2">
+                <ArrowUp className="h-4 w-4" />
+              </div>
+              <div className="bg-white rounded-lg p-2">
+                <ArrowDown className="h-4 w-4" />
+              </div>
+              <span className="font-semibold">to navigate</span>
+              <div className="bg-white rounded-lg p-2">esc</div>
+              <span className="font-semibold">to close</span>
+            </div>
+            <div className="flex items-center space-x-4">
+              <Button variant={"ghost"} className="flex items-center">
+                <Settings className="mr-2 h-6 w-6" />
+                <span className="font-semibold">Manage Accounts</span>
+              </Button>
+              <Button variant={"ghost"} className="flex items-center">
+                <Plus className="mr-2 h-6 w-6" />
+                <span className="font-semibold">Create New</span>
+              </Button>
+            </div>
+          </div>
         </div>
       </DialogContent>
     </>
@@ -108,7 +119,7 @@ interface Account {
 
 function AccountItem({ account }: Account) {
   return (
-    <div className="flex items-center space-x-4">
+    <div className="flex items-center space-x-4 hover:cursor-pointer hover:bg-kindsGrey px-6 py-3">
       <Avatar>
         <div className="bg-primary text-primary-foreground flex h-full w-full items-center justify-center text-sm font-medium bg-white">
           <Image src={account.icon} alt="" className="w-8 h-8" />
