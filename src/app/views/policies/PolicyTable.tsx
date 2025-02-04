cf78021bae65f405df65c879fd89ff3da863e1dc
Earl Pappi <earlpappi@gmail.com>
2025-02-04 19:37:17 +0100

    update

--- src/app/views/policies/PolicyTable.tsx
+++ src/app/views/policies/PolicyTable.tsx
@@ -25,56 +25,7 @@ import { useFetchAllAccountsQuery } from "@/api/m365/accounts";
 import { useFetchAllPolicyQuery } from "@/api/m365/policy";
 import TableSkeleton from "@/components/global/table-loading-state";
 
-const policies = [
-  {
-    id: 1,
-    name: "Require authentication for files after 60 days",
-    description:
-      "Set the maximum time data can be accessed without additional security layers required to access content.",
-    status: "active",
-    coverage: { accounts: 32, users: 112 },
-    createdBy: "Phoenix Baker",
-    created: "12/23/2023",
-  },
-  {
-    id: 2,
-    name: "Max session length for encrypted content",
-    description:
-      "Set the frequency that end users will have to re-authenticate to access encrypted messages and files.",
-    status: "active",
-    coverage: { accounts: 32, users: 112 },
-    createdBy: "Phoenix Baker",
-    created: "12/23/2023",
-  },
-  {
-    id: 3,
-    name: "2FA Method",
-    description: "DUO is your default 2FA method",
-    status: "inactive",
-    coverage: { accounts: 32, users: 112 },
-    createdBy: "Phoenix Baker",
-    created: "12/23/2023",
-  },
-  {
-    id: 4,
-    name: "Login requirements for business user",
-    description: "DUO is required to access encrypted content",
-    status: "active",
-    coverage: { accounts: 32, users: 112 },
-    createdBy: "Phoenix Baker",
-    created: "12/23/2023",
-  },
-  {
-    id: 5,
-    name: "Content filtering",
-    description:
-      "Automatically encrypt email based on message content, attached content, or recipient. You can set custom values.",
-    status: "inactive",
-    coverage: { accounts: 32, users: 112 },
-    createdBy: "Phoenix Baker",
-    created: "12/23/2023",
-  },
-];
+
 
 export default function PolicyTable({ filter, setFilter }: PolicyPageProps) {
   const token = useAppSelector((store) => store.authState.token);
@@ -85,7 +36,9 @@ export default function PolicyTable({ filter, setFilter }: PolicyPageProps) {
     isError: accountDetailsError,
   } = useFetchAllAccountsQuery();
 
-  const orgId = accountDetails?.organizations[0].id || "";
+  console.log("ACCOUNTDETAILS", accountDetails)
+  console.log("ACCOUNTDETAILS", accountDetailsError)
+  const orgId = accountDetails?.organizations[0]?.id ?? "";
 
   const {
     data: policyData,
